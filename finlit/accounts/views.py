ace032771fc107dc175f6c0fac56cf68e78598fa
Atharva1512 <borawakeatharva2002@gmail.com>
2021-09-10 18:23:07 +0530

    Registration And Log In complete

--- finlit/accounts/views.py
+++ finlit/accounts/views.py
@@ -1,6 +1,26 @@
+from django.contrib.messages.api import error
+from django.core.checks import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
+from django.contrib import messages 
 # Create your views here.
+
+
+def login(request):
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            messages.info(request,"Invalid credentials")
+            return redirect("login")
+    else:
+        return render(request,"login.html")
 def register(request):
 
     if request.method == "POST":
@@ -11,11 +31,27 @@ def register(request):
         password1=request.POST['password1']
         password2=request.POST['password2']
 
-        user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
-        user.save();
-        return redirect("/")
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,"Username Taken")
+                return redirect("register")
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,"Email Taken")
+                return redirect("register")
+            else:
+                user=User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
+                user.save();
+                return redirect("login")    
+        else:
+            messages.info(request,"Password not matching.")
+            return redirect("register")
     else:
         return render(request,'register.html')
 
 def index(request):
     return render(request,'index.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
